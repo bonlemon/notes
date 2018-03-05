b2cd39e6385d2025c0aaedc942297cdc7d19d82a
Avilov Dmitry <AvilovDI@alfastrah.ru>
2018-03-05 20:02:54 +0300

    check adaptive

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,12 +8,21 @@ import './App.scss';
 class App extends React.Component {
 
     render (){
+            // <div className="app">
+            //     <h2 className="app__header">NotesApp</h2>
+            //     <ContainerNoteEditor />
+            //     <Note />
+            //     <NoteGrid />
+            // </div>
         return (
+            
             <div className="app">
-                <h2 className="app__header">NotesApp</h2>
-                <ContainerNoteEditor />
-                <Note />
-                <NoteGrid />
+                <div className="app__sidebar">
+                    <img src="http://localhost:8080/client/imgs/try.png" alt="" className="app__flexible-image "/>
+                </div>
+                <div className="app__main">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas mollitia eveniet beatae maiores numquam consequuntur quae fuga ratione non, quasi aliquid ipsum delectus! Ipsa dicta sint dolorum nesciunt necessitatibus qui!
+                </div>
             </div>
         )
     }
